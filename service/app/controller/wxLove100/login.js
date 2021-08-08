d965c51a7aedc30d64bb297ca50103f295f518a9
ChenLiang <236338364@qq.com>
2021-08-08 18:05:14 +0800

    bug fixed

--- service/app/controller/wxLove100/login.js
+++ service/app/controller/wxLove100/login.js
@@ -15,11 +15,13 @@ class HomeController extends Controller {
         `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${Love100Config.appid}&secret=${Love100Config.secret}`
       )
       const openid = res.data.openid
-      let userInfo = await app.mysql.query(`SELECT a.*, b.nickName as loverNickName, b.avatarUrl as loverAvatarUrl 
-        FROM love100_user a, love100_user b WHERE a.id = ? AND a.lover = b.id`, [openid])
+      let userInfo = await app.mysql.query(`SELECT * FROM love100_user WHERE id = ?`, [openid])
       if (userInfo.length === 0) {
         await app.mysql.query(`INSERT INTO love100_user (id, time) VALUES (?, ?)`, [openid, Date.now()])
         userInfo = [{ id: openid }]
+      } else {
+        userInfo = await app.mysql.query(`SELECT a.*, b.nickName as loverNickName, b.avatarUrl as loverAvatarUrl 
+        FROM love100_user a, love100_user b WHERE a.id = ? AND a.lover = b.id`, [openid])
       }
       ctx.body = { userInfo: userInfo[0], session_key: res.data.session_key, access_token: accessTokenRes.data.access_token }
     } catch(e) {
