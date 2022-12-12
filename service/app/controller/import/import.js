8f02ee9579a461c7003f80fbb091c64f40a5d6db
LeonChen <236338364@qq.com>
2022-12-12 16:04:01 +0800

    删除server文件夹

--- service/app/controller/import/import.js
+++ service/app/controller/import/import.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path')
 const fs = require('fs')
-const filePath = path.join(__dirname, '../../../../server/import')
+const filePath = path.join(__dirname, '../../../../import')
 const shortid = require('shortid')
 
 const Controller = require('egg').Controller;
