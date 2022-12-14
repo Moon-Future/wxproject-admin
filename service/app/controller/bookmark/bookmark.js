5660ed827e3f988050f38542639b84afefb98c71
Leon <236338364@qq.com>
2022-12-15 00:19:51 +0800

    排序

--- service/app/controller/bookmark/bookmark.js
+++ service/app/controller/bookmark/bookmark.js
@@ -7,22 +7,27 @@ class HomeController extends Controller {
     const conn = await app.mysql.beginTransaction()
     try {
       const { bookmarks } = ctx.request.body
-      let insetSql = 'INSERT INTO bookmark_bookmark (id, web_name, web_url, icon_url, folder_status, parent_id, sort_number, user_id) VALUES '
-      let fields = []
-      let data = []
+      const insertData = []
       for (let key in bookmarks) {
         const item = bookmarks[key]
-        const arr = [item.id, item.label, item.url || '', item.icon || '', item.folder ? 1 : 0, item.parentId, item.sort, '8023']
-        const list = ['?', '?', '?', '?', '?', '?', '?', '?']
-        data.push(...arr)
-        fields.push(`(${list.join(',')})`)
+        insertData.push({
+          id: item.id,
+          web_name: item.label,
+          web_url: item.url || '',
+          icon_url: item.icon || '',
+          folder_status: item.folder ? 1 : 0,
+          parent_id: item.parentId,
+          sort_number: item.sort,
+          user_id: '8023',
+        })
       }
-      await conn.query(`${insetSql}${fields.join(',')}`, data)
+      await conn.insert('bookmark_bookmark', insertData)
       await conn.commit()
       ctx.body = { status: 1 }
     } catch (e) {
       await conn.rollback()
       console.log(e)
+      ctx.status = 500
       ctx.body = { message: '服务端出错' }
     }
   }
@@ -33,12 +38,41 @@ class HomeController extends Controller {
     try {
       const { bookmark } = ctx.request.body
       const { id, name, url, icon } = bookmark
-      await conn.query(`UPDATE bookmark_bookmark SET web_name = ?, web_url = ?, icon_url = ? WHERE id = ?`, [name, url, icon, id])
+      const updateData = { id, web_name: name, web_url: url, icon_url: icon }
+      await conn.update('bookmark_bookmark', updateData)
       await conn.commit()
       ctx.body = { status: 1 }
     } catch (e) {
       await conn.rollback()
       console.log(e)
+      ctx.status = 500
+      ctx.body = { message: '服务端出错' }
+    }
+  }
+
+  async moveBookmark() {
+    const { ctx, app } = this
+    const conn = await app.mysql.beginTransaction()
+    try {
+      const { bookmark, parentID } = ctx.request.body
+      const { id, folder } = bookmark
+      let sort = 1
+      const res = await conn.select('bookmark_bookmark', {
+        where: { parent_id: parentID }
+      })
+      if (res.length) {
+        res.sort((a, b) => {
+          return a.sort_number - b.sort_number
+        })
+        sort = res[res.length - 1].sort_number + 1
+      }
+      await conn.update('bookmark_bookmark', { id, parent_id: parentID, sort_number: sort })
+      await conn.commit()
+      ctx.body = { status: 1 }
+    } catch (e) {
+      await conn.rollback()
+      console.log(e)
+      ctx.status = 500
       ctx.body = { message: '服务端出错' }
     }
   }
@@ -49,12 +83,53 @@ class HomeController extends Controller {
     try {
       const { bookmark } = ctx.request.body
       const { id, folder } = bookmark
-      await conn.query(`UPDATE bookmark_bookmark SET delete_status = 1 WHERE id = ?`, [id])
+      let folderStatus = !!folder
+      let ids = [id]
+      await conn.update('bookmark_bookmark', { id, delete_status: 1 })
+      while (folderStatus) {
+        const res = await conn.select('bookmark_bookmark', {
+          where: { delete_status: 0, parent_id: ids },
+        })
+        folderStatus = false
+        ids = []
+        for (let i = 0, len = res.length; i < len; i++) {
+          if (res[i].folder_status) {
+            folderStatus = true
+          }
+          ids.push(res[i].id)
+        }
+        await conn.update(
+          'bookmark_bookmark',
+          { delete_status: 1 },
+          {
+            where: { id: ids },
+          }
+        )
+      }
       await conn.commit()
       ctx.body = { status: 1 }
     } catch (e) {
       await conn.rollback()
       console.log(e)
+      ctx.status = 500
+      ctx.body = { message: '服务端出错' }
+    }
+  }
+
+  async sortBookmark() {
+    const { ctx, app } = this
+    const conn = await app.mysql.beginTransaction()
+    try {
+      const { sortMap } = ctx.request.body
+      for (let key in sortMap) {
+        await conn.update('bookmark_bookmark', { id: key, sort_number: sortMap[key] })
+      }
+      await conn.commit()
+      ctx.body = { status: 1 }
+    } catch (e) {
+      await conn.rollback()
+      console.log(e)
+      ctx.status = 500
       ctx.body = { message: '服务端出错' }
     }
   }
@@ -64,12 +139,15 @@ class HomeController extends Controller {
     const conn = await app.mysql.beginTransaction()
     try {
       const { userId } = ctx.request.body
-      const res = await conn.query(`SELECT * FROM bookmark_bookmark WHERE delete_status != 1`)
+      const res = await conn.select('bookmark_bookmark', {
+        where: { delete_status: 0 }
+      })
       await conn.commit()
       ctx.body = { status: 1, data: res }
     } catch (err) {
       await conn.rollback()
       console.log(e)
+      ctx.status = 500
       ctx.body = { message: '服务端出错' }
     }
   }
--- service/app/router/bookmark.js
+++ service/app/router/bookmark.js
@@ -1,7 +1,9 @@
 module.exports = function (router, controller) {
   router.post('/api/bookmark/importBookmark/', controller.bookmark.bookmark.importBookmark)
   router.post('/api/bookmark/modifyBookmark/', controller.bookmark.bookmark.modifyBookmark)
+  router.post('/api/bookmark/moveBookmark/', controller.bookmark.bookmark.moveBookmark)
   router.post('/api/bookmark/removeBookmark/', controller.bookmark.bookmark.removeBookmark)
+  router.post('/api/bookmark/sortBookmark/', controller.bookmark.bookmark.sortBookmark)
   router.post('/api/bookmark/getBookmark/', controller.bookmark.bookmark.getBookmark)
   router.post('/api/bookmark/addBookmark/', controller.bookmark.bookmark.addBookmark)
 }
